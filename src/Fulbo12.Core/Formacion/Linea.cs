f4767d52dae57415e7a1c75ec7bedeb938bd594b
ArturoCruz606 <ar7ur0321@gmail.com>
2021-11-03 18:25:13 -0300

    Agregacion de clases formacion

---
+++ src/Fulbo12.Core/Formacion/Linea.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+namespace Fulbo12.Core.Formacion
+{
+    public class Linea
+    {
+        public List<PosicionEnCancha> PosicionesEnCancha { get; set; }
+        public Linea() =>PosicionesEnCancha = new List<PosicionEnCancha>();
+
+    }
+}
